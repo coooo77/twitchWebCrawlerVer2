94966dee5989a1418f5e125d565ac8bf61b553fa
TL Chang <coooo77@hotmail.com>
2020-11-16 20:38:40 +0800

    [Optimization] reduce cache consumption

--- twitch.js
+++ twitch.js
@@ -4,14 +4,33 @@ const { startToMonitor, timeAnnounce } = twitch
 const { announcer } = require('./util/helper')
 const app = require('./app')
 const puppeteer = require('puppeteer-core');
+
+function runningApp(count, browser) {
+  return new Promise((resolve, reject) => {
+    try {
+      let test = setInterval(async function () {
+        announcer(timeAnnounce(count++), 'time')
+        await app(browser)
+        if (count === 1000) {
+          clearInterval(test)
+          test = null
+          resolve(count)
+        }
+      }, checkStreamInterval)
+    } catch (error) {
+      console.log(error)
+      reject()
+    }
+  })
+}
+
 (async () => {
   announcer(startToMonitor)
   let count = 1
   announcer(timeAnnounce(count++), 'time')
   const browser = await puppeteer.launch(puppeteerSetting);
   await app(browser)
-  setInterval(async function () {
-    announcer(timeAnnounce(count++), 'time')
-    await app(browser)
-  }, checkStreamInterval)
+  while (true) {
+    count = await runningApp(count, browser)
+  }
 })()
